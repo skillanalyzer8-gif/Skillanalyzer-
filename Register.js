dffa8d44bf936fa7bb756bcbac31eef0f48391a4
skillanalyzer8-gif <skillanalyzer8@gmail.com>
2026-07-27 08:19:52 +0530

    register js page

---
+++ Register.js
@@ -0,0 +1,10 @@
+document.querySelector("form").addEventListener("submit",function(e){
+
+        e.preventDefault();
+
+            alert("Registration Successful!");
+
+                window.location.href="Login.html";
+
+                });
+})
