8f4c6f2cf3c361b75c2919505c7033a8f626c0c8
skillanalyzer8-gif <skillanalyzer8@gmail.com>
2026-08-05 18:34:03 -0700

    updaed

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 const firebaseConfig = {
-      apiKey:"AIzaSyAZVt5Y4OVUPMZel0oARdtkXlZt8L-ai34"
+      apiKey:"AIzaSyAZVt5Y4OVUPMZel0oARdtkXlZt8L-ai34",
         authDomain:"skillanalyzer-373ae.firebaseapp.com",
-          projectId: "skillanalyzer-373ae"
+          projectId: "skillanalyzer-373ae",
             storageBucket: "skillanalyzer-373ae.firebasestorage.app",
-              messagingSenderId: "952710822091"
-                appId: "1:952710822091:web:0f94738430db44219f834e"
+              messagingSenderId: "952710822091",
+                appId: "1:952710822091:web:0f94738430db44219f834e",
                 };
                 const app = initializeApp(firebaseConfig);
 
