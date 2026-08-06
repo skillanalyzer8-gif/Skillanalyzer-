56be49b732a811ea17e35984723aa09d252f996e
skillanalyzer8-gif <skillanalyzer8@gmail.com>
2026-08-06 14:33:05 +0530

    updated

--- Login.js
+++ Login.js
@@ -1,64 +1,56 @@
+
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 // Show / Hide Password
-
 function togglePassword() {
-
     let password = document.getElementById("password");
 
     if (password.type === "password") {
-
         password.type = "text";
-
     } else {
-
         password.type = "password";
-
     }
-
 }
 
+window.togglePassword = togglePassword;
 
-// Login Button
+// Login
+async function login() {
 
-function login() {
-
-    let email = document.querySelector('input[type="email"]').value;
-
+    let email = document.getElementById("email").value.trim();
     let password = document.getElementById("password").value;
 
-
     if (email === "" || password === "") {
-
         alert("Please enter Email and Password.");
-
         return;
+    }
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("Login Successful!");
+
+        window.location.href = "Dashboard.html";
+
+    } catch (error) {
+
+        alert(error.message);
 
     }
 
-
-    // Dummy Login
-
-    alert("Login Successful!");
-
-    window.location.href = "Dashboard.html";
-
 }
 
+window.login = login;
 
 // Forgot Password
-
 document.querySelector(".forgot").addEventListener("click", function(e){
-
     e.preventDefault();
-
     alert("Forgot Password page will be added soon.");
-
 });
 
-
 // Sign Up
-
 document.querySelector(".signup a").addEventListener("click", function(){
-
-    window.location.href="Register.html";
-
+    window.location.href = "Register.html";
 });
