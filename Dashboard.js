055e607f432b1393bf55f93d7ac2ab3696ea5e5e
skillanalyzer8-gif <skillanalyzer8@gmail.com>
2026-08-16 14:54:28 +0530

    updated

--- Dashboard.js
+++ Dashboard.js
@@ -1,3 +1,21 @@
-document.querySelector(".banner button").addEventListener("click", function () {
-            alert("Assessment will start soon!");
+// Start Journey
+document.getElementById("startJourneyBtn").addEventListener("click", function () {
+    window.location.href = "DeveloperMindset.html";
+    });
+
+    // Mission Categories
+    document.getElementById("softwareCard").addEventListener("click", function () {
+        window.location.href = "DeveloperMindset.html";
+        });
+
+        document.getElementById("uiCard").addEventListener("click", function () {
+            window.location.href = "Ui1.html";
             });
+
+            document.getElementById("entrepreneurCard").addEventListener("click", function () {
+                window.location.href = "Enter1.html";
+                });
+
+                document.getElementById("leadershipCard").addEventListener("click", function () {
+                    window.location.href = "Lead1.html";
+                    });
