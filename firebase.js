b2485a891ea15b10af38cb7d10ab4058459714be
skillanalyzer8-gif <skillanalyzer8@gmail.com>
2026-09-09 15:25:00 +0530

    update

--- firebase.js
+++ firebase.js
@@ -1,18 +1,59 @@
+// firebase.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+    getFunctions
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-functions.js";
+
+
 const firebaseConfig = {
-      apiKey:"AIzaSyAZVt5Y4OVUPMZel0oARdtkXlZt8L-ai34",
-        authDomain:"skillanalyzer-373ae.firebaseapp.com",
-          projectId: "skillanalyzer-373ae",
-            storageBucket: "skillanalyzer-373ae.firebasestorage.app",
-              messagingSenderId: "952710822091",
-                appId: "1:952710822091:web:0f94738430db44219f834e",
-                };
-                const app = initializeApp(firebaseConfig);
 
-                const auth = getAuth(app);
+    apiKey: "AIzaSyAZVt5Y4OVUPMZel0oARdtkXlZt8L-ai34",
 
-                const db = getFirestore(app);
+    authDomain: "skillanalyzer-373ae.firebaseapp.com",
 
-                export { auth, db };
+    projectId: "skillanalyzer-373ae",
+
+    storageBucket: "skillanalyzer-373ae.firebasestorage.app",
+
+    messagingSenderId: "952710822091",
+
+    appId: "1:952710822091:web:0f94738430db44219f834e"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Firebase Authentication
+const auth = getAuth(app);
+
+
+// Firestore Database
+const db = getFirestore(app);
+
+
+// Firebase Cloud Functions
+const functions = getFunctions(
+    app,
+    "asia-south1"
+);
+
+
+// Export everything
+export {
+    app,
+    auth,
+    db,
+    functions
+};
