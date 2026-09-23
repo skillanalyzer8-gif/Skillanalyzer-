28d3c456dc98039e4f46d11add3c624dc1adcff9
skillanalyzer8-gif <skillanalyzer8@gmail.com>
2026-09-24 00:12:42 +0530

    updated

--- AIChat.js
+++ AIChat.js
@@ -21,7 +21,7 @@ import {
 // ============================================
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAZVt5Y4OVUPMZel0oARdtKxZlT8L-ai34",
+apiKey: "AIzaSyAZVt5Y4OVUPMZel0oARdtKxZlZt8L-ai34",,
     authDomain: "skillanalyzer-373ae.firebaseapp.com",
     projectId: "skillanalyzer-373ae",
     storageBucket: "skillanalyzer-373ae.firebasestorage.app",
